211b93bd162b98471a47173f296c6c01b7684bb1
dankl <dankl@comfylight.com>
2017-06-07 00:42:21 +0200

    Added tests for mustBeSquarish

--- NewsReaderLib/src/test/kotlin/net/dankito/newsreader/favicon/FaviconSorterTest.kt
+++ NewsReaderLib/src/test/kotlin/net/dankito/newsreader/favicon/FaviconSorterTest.kt
@@ -36,6 +36,42 @@ class FaviconSorterTest {
     }
 
     @Test
+    fun getBestIconForGuardianIncludingRssIcon() {
+        val bestIcon = Favicon("https://assets.guim.co.uk/images/guardian-logo-rss.c45beb1bafa34b347ac333af2e6fe23f.png", FaviconType.Icon, Size(250, 40))
+        val favicons = listOf<Favicon>(Favicon("https://assets.guim.co.uk/images/favicons/451963ac2e23633472bf48e2856d3f04/152x152.png", FaviconType.AppleTouch, Size(152, 152)),
+                Favicon("https://assets.guim.co.uk/images/favicons/1a3f98d8491f8cfdc224089b785da86b/144x144.png", FaviconType.AppleTouch, Size(144, 144)),
+                Favicon("https://assets.guim.co.uk/images/favicons/cf23080600002e50f5869c72f5a904bd/120x120.png", FaviconType.AppleTouch, Size(120, 120)),
+                Favicon("https://assets.guim.co.uk/images/favicons/f438f6041a4c1d0289e6debd112880c2/114x114.png", FaviconType.AppleTouch, Size(114, 114)),
+                Favicon("https://assets.guim.co.uk/images/favicons/b5050517955e7cf1e493ccc53e64ca05/72x72.png", FaviconType.AppleTouch, Size(72, 72)),
+                Favicon("https://assets.guim.co.uk/images/favicons/4fd650035a2cebafea4e210990874c64/57x57.png", FaviconType.AppleTouchPrecomposed),
+                Favicon("https://assets.guim.co.uk/images/favicons/79d7ab5a729562cebca9c6a13c324f0e/32x32.ico", FaviconType.ShortcutIcon, type = "image/png"),
+                Favicon("https://assets.guim.co.uk/images/favicons/f06f6996e193d1ddcd614ea852322d25/windows_tile_144_b.png", FaviconType.MsTileImage),
+                Favicon("https://assets.guim.co.uk/images/2170b16eb045a34f8c79761b203627b4/fallback-logo.png", FaviconType.OpenGraphImage),
+                bestIcon)
+
+        val result = underTest.getBestIcon(favicons, mustBeSquarish = false)
+        assertThat(result, `is`(bestIcon))
+    }
+
+    @Test
+    fun getBestSquareIconForGuardianIncludingRssIcon() {
+        val bestIcon = Favicon("https://assets.guim.co.uk/images/favicons/451963ac2e23633472bf48e2856d3f04/152x152.png", FaviconType.AppleTouch, Size(152, 152))
+        val favicons = listOf<Favicon>(Favicon("https://assets.guim.co.uk/images/guardian-logo-rss.c45beb1bafa34b347ac333af2e6fe23f.png", FaviconType.Icon, Size(250, 40)),
+                Favicon("https://assets.guim.co.uk/images/favicons/1a3f98d8491f8cfdc224089b785da86b/144x144.png", FaviconType.AppleTouch, Size(144, 144)),
+                Favicon("https://assets.guim.co.uk/images/favicons/cf23080600002e50f5869c72f5a904bd/120x120.png", FaviconType.AppleTouch, Size(120, 120)),
+                Favicon("https://assets.guim.co.uk/images/favicons/f438f6041a4c1d0289e6debd112880c2/114x114.png", FaviconType.AppleTouch, Size(114, 114)),
+                Favicon("https://assets.guim.co.uk/images/favicons/b5050517955e7cf1e493ccc53e64ca05/72x72.png", FaviconType.AppleTouch, Size(72, 72)),
+                Favicon("https://assets.guim.co.uk/images/favicons/4fd650035a2cebafea4e210990874c64/57x57.png", FaviconType.AppleTouchPrecomposed),
+                Favicon("https://assets.guim.co.uk/images/favicons/79d7ab5a729562cebca9c6a13c324f0e/32x32.ico", FaviconType.ShortcutIcon, type = "image/png"),
+                Favicon("https://assets.guim.co.uk/images/favicons/f06f6996e193d1ddcd614ea852322d25/windows_tile_144_b.png", FaviconType.MsTileImage),
+                Favicon("https://assets.guim.co.uk/images/2170b16eb045a34f8c79761b203627b4/fallback-logo.png", FaviconType.OpenGraphImage),
+                bestIcon)
+
+        val result = underTest.getBestIcon(favicons, mustBeSquarish = true)
+        assertThat(result, `is`(bestIcon))
+    }
+
+    @Test
     fun getBestIconForNewYorkTimes() {
         val bestIcon = Favicon("https://static01.nyt.com/images/icons/t_logo_291_black.png", FaviconType.OpenGraphImage)
         val favicons = listOf<Favicon>(Favicon("https://static01.nyt.com/favicon.ico", FaviconType.ShortcutIcon),
@@ -71,6 +107,41 @@ class FaviconSorterTest {
                 bestIcon)
 
         val result = underTest.getBestIcon(favicons, minSize = 32, maxSize = 112)
+        assertThat(result, `is`(bestIcon))
+    }
+
+    @Test
+    fun getBestIconForDieZeit() {
+        val bestIcon = Favicon("http://img.zeit.de/static/img/zo-icon-win8-144x144.png", FaviconType.MsTileImage)
+        val favicons = listOf<Favicon>(Favicon("http://www.zeit.de/favicon.ico", FaviconType.ShortcutIcon),
+                Favicon("http://img.zeit.de/static/img/ZO-ipad-114x114.png", FaviconType.AppleTouchPrecomposed),
+                bestIcon)
+
+        val result = underTest.getBestIcon(favicons)
+        assertThat(result, `is`(bestIcon))
+    }
+
+    @Test
+    fun getBestIconForDieZeitIncludingRssIcon() {
+        val bestIcon = Favicon("http://img.zeit.de/bilder/elemente_01_06/logos/homepage_top.gif", FaviconType.Icon)
+        val favicons = listOf<Favicon>(Favicon("http://img.zeit.de/static/img/zo-icon-win8-144x144.png", FaviconType.MsTileImage),
+                Favicon("http://www.zeit.de/favicon.ico", FaviconType.ShortcutIcon),
+                Favicon("http://img.zeit.de/static/img/ZO-ipad-114x114.png", FaviconType.AppleTouchPrecomposed),
+                bestIcon)
+
+        val result = underTest.getBestIcon(favicons)
+        assertThat(result, `is`(bestIcon))
+    }
+
+    @Test
+    fun getBestSquareIconForDieZeitIncludingRssIcon() {
+        val bestIcon = Favicon("http://img.zeit.de/static/img/zo-icon-win8-144x144.png", FaviconType.MsTileImage)
+        val favicons = listOf<Favicon>(Favicon("http://img.zeit.de/bilder/elemente_01_06/logos/homepage_top.gif", FaviconType.Icon),
+                Favicon("http://www.zeit.de/favicon.ico", FaviconType.ShortcutIcon),
+                Favicon("http://img.zeit.de/static/img/ZO-ipad-114x114.png", FaviconType.AppleTouchPrecomposed),
+                bestIcon)
+
+        val result = underTest.getBestIcon(favicons, mustBeSquarish = true)
         assertThat(result, `is`(bestIcon))
     }
 
