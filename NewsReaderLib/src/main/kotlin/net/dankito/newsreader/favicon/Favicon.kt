50625dbc79f5a9ccb6398f31d5d1351b8b4358e8
dankl <dankl@comfylight.com>
2017-06-07 11:43:43 +0200

    Removed now unused setSizeFromString()

--- NewsReaderLib/src/main/kotlin/net/dankito/newsreader/favicon/Favicon.kt
+++ NewsReaderLib/src/main/kotlin/net/dankito/newsreader/favicon/Favicon.kt
@@ -3,11 +3,4 @@ package net.dankito.newsreader.favicon
 
 data class Favicon(val url : String, val iconType : FaviconType, var size : Size? = null, val type : String? = null) {
 
-    fun setSizeFromString(sizeString: String) {
-        val parts = sizeString.split("x")
-        if(parts.size == 2) {
-            this.size = Size(parts[0].toInt(), parts[1].toInt())
-        }
-    }
-
 }
