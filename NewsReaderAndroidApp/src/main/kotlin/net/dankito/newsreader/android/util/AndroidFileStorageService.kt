0bcf2a336fec0d3a06fc8d5883d05bbe8f54d184
dankl <dankl@comfylight.com>
2017-06-07 21:12:24 +0200

    Converted AndroidFileStorageService to a Kotlin file

---
+++ NewsReaderAndroidApp/src/main/kotlin/net/dankito/newsreader/android/util/AndroidFileStorageService.kt
@@ -0,0 +1,29 @@
+package net.dankito.newsreader.android.util
+
+import android.content.Context
+import net.dankito.newsreader.util.JavaFileStorageService
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.InputStream
+import java.io.OutputStream
+
+
+class AndroidFileStorageService(val context: Context) : JavaFileStorageService() {
+
+
+    override fun getFileInDataFolder(filename: String): File {
+        return File(context.filesDir, filename)
+    }
+
+
+    @Throws(FileNotFoundException::class)
+    override fun createFileOutputStream(filename: String): OutputStream {
+        return context.openFileOutput(filename, Context.MODE_PRIVATE)
+    }
+
+    @Throws(FileNotFoundException::class)
+    override fun createFileInputStream(filename: String): InputStream {
+        return context.openFileInput(filename)
+    }
+
+}
